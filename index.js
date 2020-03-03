aa282bdad1f0b804237d264db4ee8d40720b639a
Theo Booth <theorlbooth@googlemail.com>
2020-03-03 17:59:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i=0; i < array.length; i++) {
-    if(array[i] === findFn() {
-      return findFn()
+    if(array[i] === findFn {
+      return findFn
     } else {
       return null
     }
