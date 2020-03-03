a96e85064f50cc935feae92e6000d5176f8336ca
Theo Booth <theorlbooth@googlemail.com>
2020-03-03 17:58:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i=0; i < array.length; i++) {
-    if(array[i] === findFn(n)) {
-      return findFn(n)
+    if(array[i] === findFn() {
+      return findFn()
     } else {
       return null
     }
