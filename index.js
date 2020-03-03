25aad344d3e1435d4bb4e0bf7ab838deb15067f6
Theo Booth <theorlbooth@googlemail.com>
2020-03-03 17:23:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ function breakOut(array, changeValue, stopValue) {
     if(array[i] !== stopValue) {
     array[i] = changeValue
     } else {
-    break
+        break
   }
+}
   return array
-}}
+}
